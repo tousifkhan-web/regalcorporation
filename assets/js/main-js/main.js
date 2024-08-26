268f3ffca4eee0ac17ef0788c361bdccc91a6e9c
tousif <tousifkhanweb@gmail.com>
2024-08-26 23:24:09 +0530

    counter add

--- assets/js/main-js/main.js
+++ assets/js/main-js/main.js
@@ -296,23 +296,7 @@ $(".hambarger-mobile-menu").click(function () {
 
 // counter
 
-const counters = document.querySelectorAll(".counter");
-
-counters.forEach((counter) => {
-  counter.innerText = "0";
-
-  const updateCounter = () => {
-    const target = +counter.getAttribute("data-target");
-    const c = +counter.innerText;
-
-    const increment = target / 150;
-
-    if (c < target) {
-      counter.innerText = `${Math.ceil(c + increment)}`;
-      setTimeout(updateCounter, 50);
-    } else {
-      counter.innerText = target;
-    }
-  };
-  updateCounter();
+$(".counter").counter({
+  countFrom: 0,
+  duration: 7000,
 });
